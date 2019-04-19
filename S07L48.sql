aa90fb42d6f194aed474d4c37fb70e1cbb105901
Juan Vivas <juanlvo@gmail.com>
2019-04-19 09:32:10 +0200

    feat: Section 7 Lecture 49 pl/sql records second method
    feat: Section 7 Lecture 49 pl/sql records second method

--- S07L48.sql
+++ S07L48.sql
@@ -15,3 +15,42 @@ BEGIN
     
     dbms_output.put_line(v_emp.v_emp_id||' '||v_emp.v_first_name||' '||v_emp.v_last_name);
 END;
+--
+DECLARE
+    TYPE t_EMP IS RECORD
+    (   v_emp_id     employees.employee_id%type,
+        v_first_name employees.first_name%type,
+        v_last_name  employees.last_name%type
+    );
+    v_emp t_EMP;
+BEGIN
+    SELECT employee_id,
+            first_name,
+            last_name
+    INTO v_emp.v_emp_id, v_emp.v_first_name, v_emp.v_last_name
+    FROM employees
+    WHERE employee_id=100;
+    
+    dbms_output.put_line(v_emp.v_emp_id||' '||v_emp.v_first_name||' '||v_emp.v_last_name);
+END;
+--
+DECLARE
+    TYPE t_EMP IS RECORD
+    (   v_emp_id     employees.employee_id%type,
+        v_first_name employees.first_name%type,
+        v_last_name  employees.last_name%type
+    );
+    v_emp t_EMP;
+BEGIN
+    FOR i IN 100..103
+    LOOP
+        SELECT employee_id,
+                first_name,
+                last_name
+        INTO v_emp
+        FROM employees
+        WHERE employee_id=i;
+        
+        dbms_output.put_line(v_emp.v_emp_id||' '||v_emp.v_first_name||' '||v_emp.v_last_name);
+    END LOOP;
+END;
