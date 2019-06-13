1ccc538547d83bd90046cad1d630d394eec7bbc1
Juan Vivas <juanlvo@gmail.com>
2019-06-13 14:50:11 +0200

    feat: Section 12 Lecture 93 When and why to recompile a package?

---
+++ S12L93.sql
@@ -0,0 +1,61 @@
+
+-------------------------------------------------------------------------------
+CREATE OR REPLACE PACKAGE BODY p_test IS
+  c_var3 VARCHAR2(100):='hi there';
+  PROCEDURE print IS
+    c_var4 VARCHAR2(100):='hi';
+  BEGIN
+    DBMS_OUTPUT.PUT_LINE('this variable came from the package spec. '||c_var1);
+    DBMS_OUTPUT.PUT_LINE('this variable came from the package spec. '||c_var2);
+    DBMS_OUTPUT.PUT_LINE('this variable came from the package body. '||c_var3);
+    DBMS_OUTPUT.PUT_LINE('this variable came from the print proc '||c_var4);
+  END;
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('this is optional');
+END;
+------------------------------------------------------------------------------
+EXECUTE p_test.print;
+------------------------------------------------------------------------------
+CREATE OR REPLACE PACKAGE p_test IS
+
+  c_var1 CONSTANT NUMBER:=10;
+  c_var2 VARCHAR2(100):='welcome';
+  p_n NUMBER;
+  
+  PROCEDURE PRINT;
+
+END;
+------------------------------------------------------------------------------
+EXECUTE p_test.print;
+------------------------------------------------------------------------------
+CREATE OR REPLACE PACKAGE p_test IS
+
+  c_var1 CONSTANT NUMBER:=10;
+  c_var2 VARCHAR2(100):='welcome';
+  p_n NUMBER;
+  
+  PROCEDURE PRINT;
+  FUNCTION get_name(p NUMBER) RETURN VARCHAR2;
+
+END;
+------------------------------------------------------------------------------
+EXECUTE p_test.print;
+------------------------------------------------------------------------------
+
+SELECT *
+FROM user_objects
+WHERE object_name='P_TEST';
+
+SELECT *
+FROM USER_SOURCE
+WHERE name='P_TEST'
+AND type='PACKAGE';
+
+SELECT *
+FROM USER_SOURCE
+WHERE name='P_TEST'
+AND type='PACKAGE BODY';
+
+DROP PACKAGE p_test;
+
+DROP PACKAGE BODY p_test;
