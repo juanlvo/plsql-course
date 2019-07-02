93dc9d52c53b2a2c9065334ea212dfd7e9c29db5
Juan Vivas <juanlvo@gmail.com>
2019-07-02 14:53:28 +0200

    fix: fixing the script of the Section 16 Lecture 129

--- S16L129.sql
+++ S16L129.sql
@@ -1,4 +1,4 @@
-EXECUTE hr.add_hr_table(1, 'khaled');
-
-
-select * from hr_table;
+CREATE OR REPLACE PROCEDURE add_hr_table(p_id NUMBER, p_name VARCHAR2) AUTHID CURRENT_USER IS
+BEGIN
+  INSERT INTO hr.hr_table values (p_id, p_name);
+END;
